d48363404f189dd5fce0d8fa473cac4f98d5e0f9
Cariel Cohen <cariel.cohen@gmail.com>
2023-05-24 22:40:10 -0400

    Initial Commit: Set up repository structure and basic files
    This commit marks the initial setup of the repository for the Security Headers Scanner tool. The repository structure has been created, including the main script, configuration file, and requirements file. Additionally, a README.md file has been added with instructions on how to install and use the tool.
    
    The Security Headers Scanner is an open-source project developed by Securily to assist developers in analyzing and troubleshooting the security headers in their web applications. It aims to provide insights into the presence and configuration of security headers to prevent potential security vulnerabilities.
    
    The initial commit lays the foundation for the project, and further enhancements, bug fixes, and features will be added in subsequent commits. Stay tuned for updates and feel free to contribute to the project by submitting pull requests or reporting issues.
    
    Let's build a more secure web together!

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# securily-security-headers
-Security Headers Open Source Scanner powered by OpenAI
+# Security Headers Scanner by Securily
+![Security Headers Scanner by Securily](securily-security-headers.png)
+
+This tool has been developed by Securily to help developers troubleshoot and analyze the security headers in their web applications. By checking the presence and configuration of security headers, this scanner aims to prevent potential security vulnerabilities and protect web applications from being compromised.
+
+## Features
+
+- **Header Analysis**: The tool reads and analyzes common security headers, including Strict-Transport-Security and Content-Security-Policy, to assess their configuration.
+- **OpenAI Integration**: Utilizes the OpenAI API to provide intelligent prompts for configuring security headers.
+- **Configuration Management**: Allows users to configure and customize the severity rating, reasoning, remediation steps, and possible values for each security header.
+- **Verbose Output**: Provides detailed information and feedback during the scanning process for better understanding and troubleshooting.
+- **Configuration Persistence**: Stores the header configuration in a JSON file for easy reloading and persistence across multiple runs.
+- **Results Reporting**: Generates a JSON report of the header analysis results, including header status, severity, reasoning, and remediation steps.
+
+## Quickstart
+```shell
+   git clone https://github.com/securily/security-headers-scanner.git
+   pip install -r requirements.txt
+   ./securily-headers.sh -o <OPENAI_API_KEY> -u https://securily.com
+```
+
+## Usage
+
+1. Clone the repository: `git clone https://github.com/securily/security-headers-scanner.git`
+2. Install the required dependencies: `pip install -r requirements.txt`
+3. Obtain an API key from OpenAI by following these steps:
+   - Visit the OpenAI website: [https://openai.com](https://openai.com)
+   - Click on the "Get started" or "Sign up" button to create an account.
+   - Complete the registration process by providing the required information and agreeing to the terms of service.
+   - Once you have created an account and logged in, navigate to the OpenAI API section or dashboard.
+   - Follow the instructions provided to generate an API key for accessing the OpenAI API services.
+   - Copy the generated API key and securely store it.
+4. Run the scanner: `python security_headers_scanner.py -v -o <OPENAI_API_KEY>`
+
+Replace `<OPENAI_API_KEY>` with your actual OpenAI API key.
+
+Make sure to keep your API key confidential and avoid sharing it publicly or committing it to version control systems. It is recommended to store the API key in a secure environment, such as an environment variable or a separate configuration file.
+
+## Contributing
+
+Contributions are welcome! If you encounter any issues or have suggestions for improvements, please feel free to submit a pull request or open an issue in the GitHub repository.
+
+## License
+
+This tool is open source and licensed under the [GNU General Public License v3.0](LICENSE).
---
+++ securily-headers.sh
@@ -0,0 +1,260 @@
+#!/usr/bin/env python3
+import time
+import requests
+import json
+import re
+import datetime
+import os
+import argparse
+
+# Path to the configuration file
+config_file_path = "configuration.json"
+
+# Configure argparse for command-line arguments
+parser = argparse.ArgumentParser(description="Security Security Headers Open Source Scanner powered by OpenAI")
+parser.add_argument("-u", "--url-to-scan", type=str, required=True, help="URL to scan")
+parser.add_argument("-o", "--openai-api-key", type=str, required=True, help="OpenAI API Key")
+parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
+parser.add_argument("-f", "--force-reload", action="store_true", help="Force reload the configuration")
+args = parser.parse_args()
+
+# The OpenAI API Key
+OPENAI_API_KEY = args.openai_api_key
+# The URL to Scan
+URL = args.url_to_scan
+# List of HTTP headers
+headers_to_read = ["Strict-Transport-Security", "Content-Security-Policy", "X-Content-Type-Options", "X-Frame-Options", "X-XSS-Protection", "Referrer-Policy", "Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Access-Control-Allow-Credentials", "Access-Control-Expose-Headers", "Access-Control-Max-Age", "Content-Encoding", "Content-Length", "Content-Type", "ETag", "Last-Modified", "Server", "Vary", "WWW-Authenticate", "Public-Key-Pins", "Expect-CT", "Feature-Policy", "Cross-Origin-Resource-Policy", "Cross-Origin-Embedder-Policy", "Cross-Origin-Opener-Policy"]
+
+# Array to store configuration
+configuration = []
+
+def normalizeUrl(url):
+    if not re.match('^https?://', url):
+        url = 'https://' + url
+    return url
+
+def fix_bad_json(json_string):
+    try:
+        # Try loading the JSON string as is
+        json_data = json.loads(json_string)
+        return json_data
+
+    except json.JSONDecodeError as e:
+        if args.verbose:
+          print("Error occurred while decoding JSON: {}".format(str(e)))
+
+        # Remove the invalid control character
+        cleaned_json = ''.join(c for c in json_string if c.isprintable())
+        try:
+            json_data = json.loads(cleaned_json)
+            if args.verbose:
+              print("Successfully fixed the bad JSON.")
+            return json_data
+
+        except json.JSONDecodeError as e:
+            if args.verbose:
+              print("Error occurred while fixing the bad JSON: {}".format(str(e)))
+
+    return None
+
+
+def read_headers_from_url(url):
+    try:
+        url = normalizeUrl(url)
+        response = requests.head(url, allow_redirects=True)
+        response_headers = response.headers
+
+        headers = {}
+
+        for header in headers_to_read:
+            header_value = response_headers.get(header)
+            if header_value:
+                headers[header] = header_value
+            else:
+                headers[header] = "Not Found"
+
+        return headers
+
+    except requests.RequestException as e:
+        if args.verbose:
+          print("Error occurred while reading headers from URL: {}".format(str(e)))
+        return None
+
+
+def compare_headers_configuration(headers, configuration):
+    results = []
+
+    for header in headers_to_read:
+        header_config = next((config for config in configuration if config['name'] == header), None)
+
+        header_value = headers.get(header)
+        if header_value and (header_value.startswith("http://") or header_value.startswith("https://")):
+            try:
+                response = requests.get(header_value, verify=False)
+                if response.status_code == 200:
+                    header_value = response.text
+                    headers[header] = header_value
+                else:
+                    header_value = "Failed to download content"
+            except requests.exceptions.RequestException as e:
+                header_value = "Failed to download content: {}".format(str(e))
+
+        if header_config:
+            if header == header_config.get('name') and (header_config.get('values') and any(keyword in header_value for keyword in header_config['values'].split(', '))):
+                results.append({"name": header_config.get('name'), "value": (header_value or "n/a"), "severity": header_config.get('severity'), "reason": header_config.get('reason'), "remediation": header_config.get('remediation'), "values": header_config.get('values'), "status": "PASS"})
+            else:
+                results.append({"name": header_config.get('name'), "value": (header_value or "n/a"), "severity": header_config.get('severity'), "reason": header_config.get('reason'), "remediation": header_config.get('remediation'), "values": header_config.get('values'), "directives": header_config.get('directives'), "status": "FAIL"})
+        else:
+            results.append({"name": header, "value": (header_value or "n/a"), "severity": "", "reason": "", "remediation": "", "values": "", "directives": "", "status": "FAIL"})
+
+    return results
+
+
+def configure_headers():
+    # Define the array to store configurations
+    configuration = []
+
+    # Iterate over each header
+    for header in headers_to_read:
+        retries = 3  # Number of retries
+        while retries > 0:
+            # Define the prompt
+            prompt = 'For the following security header: {}, return a JSON object in the following format: {{"name": "Header Name", "severity": "Severity Rating", ' \
+                     '"reason": "Detailed explanation justifying the severity classification of the finding", "remediation": "Step-by-step instructions for remediation.", ' \
+                     '"values": "possible values for the content security policy header comma separated", "directives": "header directives with examples"}}'.format(header)
+
+            # Create the JSON data for the request
+            json_data = {
+                "prompt": prompt,
+                "max_tokens": 2000,
+                "temperature": 0.7,
+                "top_p": 1,
+                "n": 1,
+                "frequency_penalty": 0,
+                "presence_penalty": 0
+            }
+
+            # Send the request to OpenAI API
+            response = requests.post(
+                "https://api.openai.com/v1/engines/text-davinci-003/completions",
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer {}".format(OPENAI_API_KEY)
+                },
+                json=json_data
+            )
+
+            try:
+                os.system('clear')
+                print("Working on header {}".format(header))
+                # Remove invalid control characters from response
+                response_text = response.text
+                cleaned_response = re.sub(r'[\x00-\x1F\x7F-\x9F]', '', response_text)
+
+                # Extract JSON object from cleaned response
+                extracted_json = json.loads(cleaned_response)["choices"][0]["text"]
+
+                if extracted_json is not None:
+                    try:
+                        if args.verbose:
+                            print(extracted_json)
+                        # Convert the extracted JSON to a dictionary
+                        extracted_dict = fix_bad_json(extracted_json)
+
+                        if args.verbose:
+                            print(extracted_dict)
+
+                        # Retry the OpenAPI call if the extracted dictionary is null
+                        if extracted_dict is None:
+                            print("Failed to extract non-null value for header '{}'. Retrying the OpenAPI call...".format(header))
+                            continue
+                        # Append the extracted dictionary to the array
+                        configuration.append(extracted_dict)
+                        print("Successfully processed header {}".format(header))
+
+                        # Print the extracted dictionary
+                        if args.verbose:
+                            print("Extracted Configuration for '{}':".format(header))
+                            print(extracted_dict)
+                            print()
+
+                        # Break the retry loop if the extraction is successful
+                        break
+
+                    except json.JSONDecodeError as e:
+                        if args.verbose:
+                            print("Error occurred while decoding JSON for header '{}': {}".format(header, str(e)))
+                        retries -= 1  # Decrease the number of retries
+
+                else:
+                    if args.verbose:
+                        print("Received null value for header '{}'. Retrying...".format(header))
+                    retries -= 1  # Decrease the number of retries
+
+            except (json.JSONDecodeError, IndexError, KeyError) as e:
+                if args.verbose:
+                    print("Error occurred while extracting JSON for header '{}': {}".format(header, str(e)))
+                retries -= 1  # Decrease the number of retries
+
+            # Sleep for a moment before retrying
+            time.sleep(1)
+
+    # Save the configuration to a file
+    with open(config_file_path, "w") as file:
+        json.dump(configuration, file)
+
+
+# Example usage of read_headers_from_url()
+headers = read_headers_from_url(URL)
+if headers:
+    print("Finished reading headers from URL: {}".format(URL))
+    if args.verbose:
+        print("Headers from URL: {}".format(URL))
+        print(json.dumps(headers, indent=4))
+        print()
+
+# Configure headers using OpenAI API
+if args.force_reload or not os.path.exists(config_file_path):
+    # Call the configure_headers() function to create or reload the configuration file
+    print("Loading headers from OpenAI, this will take a while")
+    configure_headers()
+    os.system('clear')
+    print("Finished configuring headers using OpenAI API")
+else:
+    # Get the modification time of the configuration file
+    mod_time = datetime.datetime.fromtimestamp(os.path.getmtime(config_file_path))
+
+    # Calculate the time difference between now and the modification time
+    time_diff = datetime.datetime.now() - mod_time
+
+    # Check if the configuration file is older than or equal to 30 days
+    if time_diff.days >= 30:
+        # Call the configure_headers() function to update the configuration
+        print("Loading headers from OpenAI, this will take a while")
+        configure_headers()
+    else:
+        # Load the configuration from the file
+        with open(config_file_path, "r") as file:
+            configuration = json.load(file)
+        os.system('clear')
+        print("Finished loading existing configuration from file")
+
+# Compare headers and configuration
+results = compare_headers_configuration(headers, configuration)
+os.system('clear')
+print("Finished comparing headers and configuration")
+
+# Store the results in a list
+results_list = []
+for result in results:
+    results_list.append(result)
+
+# Print the results in JSON format
+results_json = json.dumps(results_list, indent=4)
+print(results_json)
+
+# Write the results to a file
+with open("results.json", "w") as file:
+    file.write(results_json)
+os.system('clear')
+print("Results have been saved to results.json")
